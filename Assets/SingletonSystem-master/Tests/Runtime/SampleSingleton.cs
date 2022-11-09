5d5e5cce6effc08bcdf8e0f3e287f63c6dfc7c6f
xreasler <j.o.n.t.e@hotmail.com>
2022-11-09 22:23:58 +0100

    Removed unused files

--- Assets/SingletonSystem-master/Tests/Runtime/SampleSingleton.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using Studious.Singleton;
-
-[Singleton(Name = "Singleton UnitTest", Persistent = true)]
-public class SampleSingleton : MonoBehaviour
-{
-    public static SampleSingleton Instance => Singleton<SampleSingleton>.Instance;
-}
